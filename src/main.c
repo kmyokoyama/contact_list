bd13735ade830f790d5665a382f7bebd9a21f4e9
Aline Grance <grance.aline@gmail.com>
2021-10-11 22:36:53 -0300

    Add print line.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv){
 
   printf("This is an example\n");
+  printf("print from another source\n");
 
   return 0;
 }
