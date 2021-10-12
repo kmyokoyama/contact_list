269e035690f005784fa96f2c7f7212762e0e0b2f
Aline Grance <grance.aline@gmail.com>
2021-10-12 00:03:29 -0300

    Fix bug

--- src/contacts.h
+++ src/contacts.h
@@ -1,9 +1,12 @@
+#define MAX_STR 50
+
 typedef struct contact {
   char name[MAX_STR];
-  char phone[MAX_STR]
+  char phone[MAX_STR];
 } CONTACT;
 
 typedef struct contacts {
   CONTACT* list;
   int size;
 } CONTACTS;
+
